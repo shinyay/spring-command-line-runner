0a9c30441ca0498504e0f17a157f8fd9fa92a6bc
shinyay <shinya.com@gmail.com>
2019-10-11 17:08:01 -0500

    Modify class name generated by initializer

--- src/main/kotlin/io/pivotal/shinyay/cmdrunner/CmdRunnerApplication.kt
+++ src/main/kotlin/io/pivotal/shinyay/cmdrunner/CmdRunnerApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class CmdrunnerApplication
+class CmdRunnerApplication
 
 fun main(args: Array<String>) {
-	runApplication<CmdrunnerApplication>(*args)
+	runApplication<CmdRunnerApplication>(*args)
 }
